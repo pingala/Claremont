50ec6c75a1cd7acdad5c385790efec3093f88ecc
Nathaniel Moon <nathaniel.c.moon@gmail.com>
2019-01-24 14:53:05 -0500

    Updated session 3

--- workshop3/get_ip.py
+++ workshop3/get_ip.py
@@ -1,20 +1,25 @@
 import requests
 import json
+import sys
+
+target_dir = sys.argv[1]
 
 def get_tor_session():
     session = requests.session()
     session.headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
-
-    # We must use socks5 protocol to use Tor
     session.proxies = {
         'http':  'socks5://127.0.0.1:9050',
         'https': 'socks5://127.0.0.1:9050'
     }
     return session
 
-print("--- Regular")
-print(json.loads(requests.get("http://ipinfo.io/json").text))
-print("--- Tor")
-print(json.loads(get_tor_session().get("http://ipinfo.io/json").text))
+
+output = {
+    "normal": json.loads(requests.get("http://ip-api.com/json").text),
+    "tor": json.loads(get_tor_session().get("http://ip-api.com/json").text)
+}
+
+with open("./%s/output.json" % (target_dir), "w") as fout:
+    fout.write(json.dumps(output, indent=2))
